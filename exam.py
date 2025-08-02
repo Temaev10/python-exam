8af546033ad9fc1054931df572f4f69461616b85
Temaev10 <Temaev10@mail.ru>
2025-08-02 19:09:16 +0300

    ferst commit

---
+++ exam.py
@@ -0,0 +1,79 @@
+# while True:
+#     num = int(input("введите число: "))
+#     if num < 0:
+#         break
+#     else:
+#         print(num ** 2)
+
+
+
+# lst = ['c#', 'java', ['rust', 'python', 'js']]
+
+# print([lst[2][1]])
+
+
+# result = []
+# for i in range(1,11):
+#     result.append(i ** 2)
+
+# print(result)
+
+
+# def nums(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+    
+# print(nums(5,6))
+
+
+
+# from random import randint
+
+# nums = []
+# for i in range(5):
+#     num = randint(1, 100)
+#     nums.append(num)
+
+# print(nums)
+
+
+
+# def summa(a, b):
+#     return a + b
+
+# print(summa(4, 4))
+
+
+# calenadr = {
+#     'январь':31 ,
+#     'февраль': 28, 
+#     'март': 31
+# }
+
+# print(calenadr['март'])
+
+
+import time 
+
+correct_login = "Ansar"
+correct_password = "12345"
+
+count = 0
+
+while True:
+    login = input("введите логин: ")
+    passorwd = input("введите пароль: ")
+
+    if login == correct_login and passorwd == correct_password:
+        print("добро пожаловать! ")
+        break
+
+    else:
+        print('не правильный логин или пароль, повторите! ')
+        count += 1
+
+        if count >= 3:
+            print('доступ заблокирован на 10 секунд')
+            time.sleep(10)        
